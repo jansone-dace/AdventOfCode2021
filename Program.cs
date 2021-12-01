06698205b8809af8895f88c1a7935ed0f22aa4df
Dace Jansone <jansone-dace@inbox.lv>
2021-12-01 16:33:43 +0000

    Day 1 part 1 solution

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace AdventOfCode2021
+﻿using System.IO;
+
+namespace AdventOfCode2021
 {
     class Program
     {
+        static int Day1_1()
+        {
+            string[] depthMeasurements;
+            int previuosDepthMeasurement, currentDepthMeasurement, increaseCount = 0;
+
+            depthMeasurements = File.ReadAllLines(@".\inputs\day1_1.txt");
+            
+            if (depthMeasurements.Length == 0)
+                return 0;
+            
+            previuosDepthMeasurement = Int32.Parse(depthMeasurements[0]);
+            for (int cur = 1; cur < depthMeasurements.Length; cur++)
+            {
+                currentDepthMeasurement = Int32.Parse(depthMeasurements[cur]);
+                if (currentDepthMeasurement > previuosDepthMeasurement)
+                    increaseCount++;
+                previuosDepthMeasurement = currentDepthMeasurement;
+            }
+
+            return increaseCount;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Day1_1());
         }
     }
 }
