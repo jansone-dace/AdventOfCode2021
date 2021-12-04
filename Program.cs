c46985fe0aeeb8c110482e983c7d8b16f1d0f209
Dace Jansone <jansone-dace@inbox.lv>
2021-12-04 16:22:34 +0000

    Day 2 part 2 done

--- Program.cs
+++ Program.cs
@@ -87,11 +87,54 @@ namespace AdventOfCode2021
             return horizontalPosition * verticalPosition;
         }
 
+        static int Day2_2()
+        {
+            string[] commands;
+            string commandType;
+            int horizontalPosition = 0, aim = 0, depth = 0, commandNumericalValue;
+
+            // Read commands from input file 
+            commands = File.ReadAllLines(@".\inputs\day2.txt");
+
+            // Iterate through each command
+            foreach (string command in commands)
+            {
+                // From each command get the command type and numerical value
+                commandType = command.Split(' ')[0];
+                commandNumericalValue = Int32.Parse(command.Split(' ')[1]);
+
+                switch (commandType)
+                {
+                    case "up":
+                        // up X decreases your aim by X units
+                        aim -= commandNumericalValue;
+                        break;
+                    case "down":
+                        // down X increases your aim by X units
+                        aim += commandNumericalValue;
+                        break;
+                    case "forward":
+                        // forward X does two things:
+                        //   It increases your horizontal position by X units.
+                        horizontalPosition += commandNumericalValue;
+                        //   It increases your depth by your aim multiplied by X.
+                        depth += (aim * commandNumericalValue);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // What do you get if you multiply your final horizontal position by your final depth?
+            return horizontalPosition * depth;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine(String.Format("Day 1 part 1: {0}", Day1_1()));
             Console.WriteLine(String.Format("Day 1 part 2: {0}", Day1_2()));
             Console.WriteLine(string.Format("Day 2 part 1: {0}", Day2_1()));
+            Console.WriteLine(string.Format("Day 2 part 2: {0}", Day2_2()));
         }
     }
 }
